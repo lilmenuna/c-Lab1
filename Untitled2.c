d448a59aab8bea6cf59b65284d57248e21bdcccb
6704062611140 <s6704062611140@email.kmutnb.ac.th>
2024-11-28 14:45:14 +0700

    ส่งครั้งที่3

--- Untitled2.c
+++ Untitled2.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 int main(){
 	int x1;
-	printf("Type a number: \n");
 	int x2;
-	printf("Type a number: \n");
-	scanf("%d",&x1);
-	scanf("%d",&x2);
+	scanf("%d %d",&x1,&x2);
 	if (x1 > x2){
 		printf("%d %d",x1,(x1-x2));
 	} else {
