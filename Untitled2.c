5dce5a021b168247b814af66250eaaaf4137ee41
6704062611140 <s6704062611140@email.kmutnb.ac.th>
2024-11-28 14:46:42 +0700

    ส่งครั้งที่ 4

--- Untitled2.c
+++ Untitled2.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main(){
 	int x1;
+	printf("Type a number: \n");
 	int x2;
-	scanf("%d %d",&x1,&x2);
+	printf("Type a number: \n");
+	scanf("%d",&x1);
+	scanf("%d",&x2);
 	if (x1 > x2){
 		printf("%d %d",x1,(x1-x2));
 	} else {
